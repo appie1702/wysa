8621e71b496684a6b49ed68df101fc82b7c33add
appie1702 <arpitabhyankar99823@gmail.com>
2025-01-29 16:55:38 +0530

    fix: search params

--- components/main-content.jsx
+++ components/main-content.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import UserDetails from "./userdetails";
 import TodoList from "./todolist";
@@ -23,31 +23,33 @@ function MainContent() {
   };
 
   return (
-    <main className="flex-1 overflow-auto bg-white px-6 pb-6 pt-24  flex flex-col items-start justify-start">
-      <div className="mb-6">
-        <button
-          className={`px-4 py-2 rounded-md text-sm font-medium transition-colors mr-2 ${
-            activeTab === "user-details"
-              ? "bg-blue-600 text-white"
-              : "border border-blue-500 text-blue-500 hover:bg-blue-50"
-          }`}
-          onClick={() => handleTabChange("user-details")}
-        >
-          User Details
-        </button>
-        <button
-          className={`px-4 py-2 rounded-md text-sm font-medium transition-colors ${
-            activeTab === "todos"
-              ? "bg-blue-600 text-white"
-              : "border border-blue-500 text-blue-500 hover:bg-blue-50"
-          }`}
-          onClick={() => handleTabChange("todos")}
-        >
-          To-dos
-        </button>
-      </div>
-      {activeTab === "user-details" ? <UserDetails /> : <TodoList />}
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="flex-1 overflow-auto bg-white px-6 pb-6 pt-24  flex flex-col items-start justify-start">
+        <div className="mb-6">
+          <button
+            className={`px-4 py-2 rounded-md text-sm font-medium transition-colors mr-2 ${
+              activeTab === "user-details"
+                ? "bg-blue-600 text-white"
+                : "border border-blue-500 text-blue-500 hover:bg-blue-50"
+            }`}
+            onClick={() => handleTabChange("user-details")}
+          >
+            User Details
+          </button>
+          <button
+            className={`px-4 py-2 rounded-md text-sm font-medium transition-colors ${
+              activeTab === "todos"
+                ? "bg-blue-600 text-white"
+                : "border border-blue-500 text-blue-500 hover:bg-blue-50"
+            }`}
+            onClick={() => handleTabChange("todos")}
+          >
+            To-dos
+          </button>
+        </div>
+        {activeTab === "user-details" ? <UserDetails /> : <TodoList />}
+      </main>
+    </Suspense>
   );
 }
 
