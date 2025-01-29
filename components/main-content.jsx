2d10659b9e255545824ca77d39967a4a6827389e
appie1702 <arpitabhyankar99823@gmail.com>
2025-01-29 16:59:05 +0530

    fix:search params

--- components/main-content.jsx
+++ components/main-content.jsx
@@ -1,29 +1,17 @@
 "use client";
 
 import React, { useState, useEffect, Suspense } from "react";
-import { useSearchParams } from "next/navigation";
 import UserDetails from "./userdetails";
 import TodoList from "./todolist";
-import useUserStore from "@/store/user-store";
 
 function MainContent() {
   const [activeTab, setActiveTab] = useState("user-details");
-  const searchParams = useSearchParams();
-  const { setSelectedUser } = useUserStore();
-
-  useEffect(() => {
-    const userId = searchParams.get("userId");
-    if (userId) {
-      setSelectedUser(Number.parseInt(userId));
-    }
-  }, [searchParams, setSelectedUser]);
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
       <main className="flex-1 overflow-auto bg-white px-6 pb-6 pt-24  flex flex-col items-start justify-start">
         <div className="mb-6">
           <button
@@ -49,7 +37,6 @@ function MainContent() {
         </div>
         {activeTab === "user-details" ? <UserDetails /> : <TodoList />}
       </main>
-    </Suspense>
   );
 }
 
